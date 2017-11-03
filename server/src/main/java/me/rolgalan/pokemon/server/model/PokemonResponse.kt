4c80e6c64f2c1f44da8f4b01ce06d4cd7fb22b49
rolgalan <rolgalan@gmail.com>
2017-11-03 23:09:59 +0100

    Create server pokemon response model

---
+++ server/src/main/java/me/rolgalan/pokemon/server/model/PokemonResponse.kt
@@ -0,0 +1,42 @@
+package me.rolgalan.pokemon.server.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Roldán Galán on 03/11/2017.
+ */
+
+data class PokemonResponse(
+        val id: Int,
+        val name: String,
+        @SerializedName("base_experience")
+        val baseExperience: Int,
+        val height: Int,
+        @SerializedName("is_default")
+        val isDefault: Boolean,
+        val order: Int,
+        val weight: Int,
+        val types: List<PokemonType>,
+        val sprites: PokemonSprites) {
+}
+
+data class PokemonSprites(
+        @SerializedName("back_default")
+        val backDefault: String,
+        @SerializedName("front_default")
+        val frontDefault: String,
+        @SerializedName("back_female")
+        val backFemale: String?,
+        @SerializedName("front_female")
+        val frontFemale: String?
+)
+
+data class PokemonType(
+        val slot: Int,
+        val type: NamedApiResource
+)
+
+data class NamedApiResource(
+        val name: String,
+        val url: String
+)
