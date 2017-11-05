60c5b86b23326877d99a29a016a3aa4ca89a2335
rolgalan <rolgalan@gmail.com>
2017-11-05 17:56:27 +0100

    Add backpack handling methods to DataProvider

--- provider/src/main/java/me/rolgalan/pokemon/provider/DataProvider.kt
+++ provider/src/main/java/me/rolgalan/pokemon/provider/DataProvider.kt
@@ -1,5 +1,6 @@
 package me.rolgalan.pokemon.provider
 
+import me.rolgalan.pokemon.model.Backpack
 import me.rolgalan.pokemon.model.Pokemon
 import me.rolgalan.pokemon.provider.boundary.PokemonBoundary
 import me.rolgalan.pokemon.server.ApiManager
@@ -12,6 +13,9 @@ import me.rolgalan.pokemon.server.ApiManager
  */
 class DataProvider private constructor() {
 
+    //Cached backpack
+    val backpack = Backpack()
+
     private object Holder {
         val INSTANCE = DataProvider()
     }
@@ -21,10 +25,40 @@ class DataProvider private constructor() {
     }
 
     fun getNewPokemon(id: Int, listener: DataInterface<Pokemon>) {
+        //New pokemons always come from the server
         ApiManager.instance.getPokemon(id, PokemonBoundary(listener))
     }
 
-    fun getBackpackPokemon(id: Int, listener: DataInterface<Pokemon>) {
-        //TODO
+    fun getBackpack(listener: DataInterface<Backpack>) {
+        //TODO load backpack from DB and cache it here in the backpack field
+        listener.onReceived(backpack)
+    }
+
+    fun catchPokemon(pokemon : Pokemon, listener: DataInterface<Boolean>) {
+        getBackpack(
+                object : DataInterface<Backpack> {
+                    override fun onReceived(data: Backpack) {
+                        backpack.addPokemon(pokemon)
+                        listener.onReceived(true)
+                    }
+
+                    override fun onError(error: String) {
+                        //retry?
+                        listener.onReceived(false)
+                    }
+                })
+    }
+
+    fun hasPokemonInBackpack(id : Int, listener: DataInterface<Boolean>) {
+        getBackpack(
+                object : DataInterface<Backpack> {
+                    override fun onReceived(data: Backpack) {
+                        listener.onReceived(data.hasPokemon(id))
+                    }
+
+                    override fun onError(error: String) {
+                        listener.onError(error)
+                    }
+                })
     }
 }
